b07ec7fb2ae954dbbbbd5913dd98f9d4095d66b6
Kevin Rangel <Krangelj@unicartagena.edu.co>
2020-04-19 10:41:23 -0500

    Minor fixes

--- main.py
+++ main.py
@@ -4,80 +4,12 @@ import discord
 from discord.ext import commands
 import settings
 
-class pokemon:
-    num: 1
-    name: "Bulbasaur"
-    types: ["Grass", "Poison"]
-    genderRatio: {
-        "M": 0.875,
-        "F": 0.125
-    }
-    baseStats: {
-        "hp": 45,
-        "atk": 49,
-        "def": 49,
-        "spa": 65,
-        "spd": 65,
-        "spe": 45,
-    }
-    abilities: {
-        "0": "Overgrow",
-        "H": "Chlorophyll"
-    }
-    heightm: 0.7
-    weightkg: 6.9
-    color: "Green"
-    tier: "LC"
-
-    def __init__(self):
-        print("Species {}".format(self.name))
-
-
-class OC:
-    def __init__(self):
-        self.name = "Null"
-        self.species = pokemon()
-        self.userID = 0
-        self.finalStats: {
-            "hp": 294,
-            "atk": 216,
-            "def": 216,
-            "spa": 251,
-            "spd": 251,
-            "spe": 207
-        }
-        print("Registered OC")
-
-    def __del__(self):
-        print("Deleted OC", self.name)
-
-    def __str__(self):
-        return '{}'.format(self.name)
-
-    # def __len__(self):
-    #    return self.tiempo
-
-    def register(self):
-        self.name = "Registered"
-
-
-class ListOC:
-    OC = []
-
-    def __init__(self, OC=[]):
-        self.OC = OC
-
-    def add(self, OC):
-        self.OC.append(OC)
-
-
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_TOKEN = os.getenv("GUILD_ID")
 
 bot = commands.Bot(command_prefix='$',
                    description='A bot that greets the user back.')
 bot.remove_command('help')
-
 
 @bot.event
 async def on_ready():
@@ -155,6 +87,5 @@ async def help(ctx, args=''):
 
     embed.set_footer(text='use {}help <page>'.format(bot.command_prefix))
     await ctx.send(embed=embed)
-
 
 bot.run(DISCORD_TOKEN)
